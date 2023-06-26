c58ad9d3130ebb013970151fd1025bab6faee4b9
Qwickset <qwickset@qwickset.com>
2023-06-26 00:35:27 -0500

    Initial Author Route

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// https://www.youtube.com/watch?v=esy4nRuShl8&list=PLZlA0Gpn_vH8jbFkBjOuFjhxANC63OmXM&index=6
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').load()
 }
@@ -5,14 +6,17 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine','ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended:false}))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
@@ -21,5 +25,6 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
 app.use('/',indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
